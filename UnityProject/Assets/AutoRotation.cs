db265656154e3572d453115c57c84d696a0b93bd
GO-psychicvrlab <go05sato@gmail.com>
2017-09-18 13:37:35 +0900

    回転用スクリプト

---
+++ UnityProject/Assets/AutoRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GO
+{
+    public class AutoRotation : MonoBehaviour
+    {
+        [SerializeField]
+        private float _angle = 90;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            transform.Rotate(new Vector3(0, 0, _angle) * Time.deltaTime, Space.Self);
+        }
+    }
+}
